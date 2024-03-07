0780a4c446229356304083be7355bfdf7b02eb53
ism4jly <ismaelpaulofreitas@gmail.com>
2024-03-07 09:40:48 -0300

    component input and login screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,72 @@
 import React from 'react'
+import logoImg from '../../assets/logo.svg'
+import { Container } from '../../components/container'
+import { Link } from 'react-router-dom'
+
+import { Input } from '../../components/input'
+import { useForm} from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+    email:z.string().email("Insira um email válido").nonempty("O campo email é obrigatório"),
+    password: z.string().nonempty("O campo senha é obrigatório")
+})
+
+type FormData = z.infer<typeof schema>
 
 export function Login() {
+
+    const { register, handleSubmit, formState: {errors} } = useForm<FormData>({
+        resolver: zodResolver(schema),
+        mode: "onChange"
+    })
+
+    function onSubmit(data: FormData){
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h1>Pagina Login</h1>
-        </div>
+        <Container>
+            <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
+                <Link to='/' className='mb-6 max-w-sm w-full'>
+                    <img 
+                        className='w-full'
+                        src={logoImg} 
+                        alt="Logo do site" 
+                    />
+                </Link>
+
+                <form 
+                    className='bg-white max-w-xl w-full rounded-lg'
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <div className='mb-3'>
+                        <Input
+                            type='email'
+                            placeholder="Digite seu email..."
+                            name="email"
+                            error={errors.email?.message}
+                            register={register}
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <Input
+                            type='password'
+                            placeholder="Digite sua senha..."
+                            name="password"
+                            error={errors.password?.message}
+                            register={register}
+                        />
+                    </div>
+
+                    <button>
+                        Acessar
+                    </button>
+                </form>
+            </div>
+        </Container>
     )
 }
 
